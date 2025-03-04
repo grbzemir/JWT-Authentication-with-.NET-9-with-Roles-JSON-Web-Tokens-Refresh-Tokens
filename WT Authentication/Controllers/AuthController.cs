22434e4d0a11709bffada5c7153d0028bdf53651
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 21:17:53 +0300

    AutJWT

--- WT Authentication/Controllers/AuthController.cs
+++ WT Authentication/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WT_Authentication.Entities;
+using WT_Authentication.Models;
 
 namespace WT_Authentication.Controllers
 {
@@ -8,6 +11,22 @@ namespace WT_Authentication.Controllers
     public class AuthController : ControllerBase
     {
 
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+
+        {
+            //Request istek atma işlemi request ile userDto'ya
+            //istek atıyorum istek başarılı olursa User nesnesi döner
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user , request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
 
     }
 }
