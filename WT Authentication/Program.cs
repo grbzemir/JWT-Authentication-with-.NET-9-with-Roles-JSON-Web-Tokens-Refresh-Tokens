91d098ff2bd4f3bfb26b0e9c21d7c0a943bbdef5
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 22:28:12 +0300

    Jwt Authentication

--- WT Authentication/Program.cs
+++ WT Authentication/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WT_Authentication.Database;
 using WT_Authentication.Services;
 
@@ -10,6 +13,21 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+     .AddJwtBearer(options =>
+     {
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateIssuer = true,
+             ValidIssuer = builder.Configuration["AppSettings:Issuer"],
+             ValidateAudience = true,
+             ValidAudience = builder.Configuration["AppSettings:Audience"],
+             ValidateLifetime = true,
+             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)),
+             ValidateIssuerSigningKey = true
+
+         };
+     });
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
