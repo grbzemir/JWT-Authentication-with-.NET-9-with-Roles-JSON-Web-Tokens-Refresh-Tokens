7d3062d895a3a537db18a162e53a7620f5d751b0
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 21:09:09 +0300

    Entities

---
+++ WT Authentication/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WT_Authentication.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;   
+
+
+    }
+}
