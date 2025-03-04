a8c343914340249774c9dd9b98ff0efb69e3773c
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 22:59:06 +0300

    Controllers

--- WT Authentication/Controllers/AuthController.cs
+++ WT Authentication/Controllers/AuthController.cs
@@ -33,13 +33,24 @@ namespace WT_Authentication.Controllers
 
         [HttpPost("login")]
 
-        public async Task<ActionResult<string>> Login(UserDto request)
+        public async Task<ActionResult<TokenResponseDto>> Login(UserDto request)
         {
-            var token = await authService.LoginAsync(request);
-            if (token is null)
+            var result = await authService.LoginAsync(request);
+            if (result is null)
                 return BadRequest("Invalid username or password.");
 
-            return Ok(token);
+            return Ok(result);
+        }
+
+        [HttpPost("refresh-tokens")]
+
+        public async Task<ActionResult<TokenResponseDto>> RefreshTokens(RefreshTokenRequestDto request)
+        {
+            var result = await authService.RefreshTokensAsync(request);
+            if (result is null || result.AccessToken is null || result.RefreshToken is null)
+                return Unauthorized("Invalid refresh token.");
+
+            return Ok(result);
         }
 
         [Authorize]
