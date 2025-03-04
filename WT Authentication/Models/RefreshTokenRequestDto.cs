2155022ed03d2703d98b1959c6c8dda4380c29a9
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 22:59:19 +0300

    Dtos

---
+++ WT Authentication/Models/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WT_Authentication.Models
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid  UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
---
+++ WT Authentication/Models/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WT_Authentication.Models
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
