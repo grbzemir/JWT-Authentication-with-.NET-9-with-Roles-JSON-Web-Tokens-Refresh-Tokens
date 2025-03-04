f659e08ac151bec087e9410112e25baf8c053449
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 21:11:37 +0300

    JWT

---
+++ WT Authentication/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WT_Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+
+    }
+}
