63ce73e1b881e9ee3e7163acac30acbe035d6a11
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-04 21:59:02 +0300

    DbContext

---
+++ WT Authentication/Database/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WT_Authentication.Entities;
+
+namespace WT_Authentication.Database
+{
+    public class UserDbContext:DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+       
+    }
+}
